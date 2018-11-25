0ec168fa1796f2aef2d7b74a7c44c8538db17c39
JasonCorp84 <csababalogh84@gmail.com>
2018-11-24 17:23:16 -0800

    Boiler plate is up

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import Game from './Game';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        TTT
+        <Game />
       </div>
     );
   }
---
+++ src/Square.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class Square extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+        <button className="square">
+
+        </button> 
+        );
+    }
+}
+ 
+export default Square;
